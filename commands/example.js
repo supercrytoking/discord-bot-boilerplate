db82664a8119477851597012d2205d41e4656d48
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-03-14 01:05:17 -0400

    Modified example command.

--- commands/example.js
+++ commands/example.js
@@ -2,9 +2,8 @@ exports.run = (client, message, args) => {
 
     // your function
 
-    message.channel.send('This is an example command!')
-    console.log('You can run this command with !example, !test, or !admin!')
+    message.channel.send(`This is an example command! You can run this command with ${client.settings.prefix}example, ${client.settings.prefix}test, or ${client.settings.prefix}admin!`)
 }
 
-exports.help = 'Just an example command. Usage: `!example`'
+exports.help = 'Just an example command. Usage: `${client.settings.prefix}example`'
 exports.aliases = ['test', 'admin']
